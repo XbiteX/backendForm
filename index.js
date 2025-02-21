10a4421bbc64695afe515e7097c24ad19483c20a
michele <michifilip2006@gmail.com>
2025-02-21 20:16:04 +0100

    refactor: clean up code by commenting out unused PORT variable and logging statements

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
+// const PORT = 3080;
+
 app.use(cors()); app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,13 +14,16 @@ app.get('/', (req, res) => {
 
 
 app.post('/utente', (req, res) => {
-    // console.log(req.body);
-    const { datiUtente } = req.body;
+    console.log(req.body);
+    const datiUtente  = req.body;
+    console.log(datiUtente);
      if (!datiUtente) { res.status(400).json({
          message: "Mancato inserimento di alcuni dati" 
         });
      } 
       res.json(datiUtente); 
     }) 
+
+// app.listen(PORT, () => { console.log("Server is running")})
 
 module.exports = app;
